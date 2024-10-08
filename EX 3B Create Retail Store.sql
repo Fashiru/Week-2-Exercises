c5ea8f1fe80ce6c48b684dac123a9f0b85cbd587
Fadl-llah Aashiru <fashiru@appdev.yearup.org>
2024-10-08 09:56:09 -0500

    added word file 4d

---
+++ EX 3B Create Retail Store.sql
@@ -0,0 +1,8 @@
+-- This is how to drop table from a datebase (Drop table Rewards_Table;)
+-- Creating new database for friends retail store:
+-- Create Database (CREATE DATABASE Retail_Store;)
+
+CREATE TABLE Rewards_Table (
+	Rewards_ID VARCHAR(10) primary key
+);
+
